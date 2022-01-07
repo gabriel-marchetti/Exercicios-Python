0be29ba787eca387cfb94974097a2589f790a474
Gabriel Cunha Marchetti <gabrielcmarchetti@gmail.com>
2022-01-06 22:38:51 -0300

    Aula

---
+++ Programação em python do básico ao avançado/pep8.py
@@ -0,0 +1,13 @@
+"""
+    PEP8 - Python Enhancement Proposal
+    São propostas de melhorias para a linguagem Python
+    
+    import this
+    
+    A ideia do PEP8 é de padronizar os códigos Python, desse modo nosso código pode ser lido por qualquer pessoa que domine a linguagem
+    
+    [1] - Utilize Camel Case para nomes de classe:
+        Um tópico de orientação a objetos
+        
+    Camel Case é um tipo de classe
+"""
